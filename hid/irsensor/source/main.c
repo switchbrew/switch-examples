500197380510888c64cd48eed62635f0823f2bb5
yellows8 <yellows8@users.noreply.github.com>
2018-02-02 16:49:07 -0500

    Added irsensor example.

---
+++ hid/irsensor/source/main.c
@@ -0,0 +1,50 @@
+#include <string.h>
+#include <stdio.h>
+
+#include <switch.h>
+
+//TODO: Implement image-transfer in libnx and in this example.
+
+int main(int argc, char **argv)
+{
+	Result rc=0;
+	u32 irhandle=0;
+
+	gfxInitDefault();
+
+	//Initialize console. Using NULL as the second argument tells the console library to use the internal console structure as current one.
+	consoleInit(NULL);
+
+	rc = irsInitialize();
+	printf("irsInitialize() returned 0x%x\n", rc);
+
+	if (R_SUCCEEDED(rc))
+	{
+		rc = irsActivateIrsensor(1);
+		printf("irsActivateIrsensor() returned 0x%x\n", rc);
+	}
+
+	if (R_SUCCEEDED(rc))
+	{
+		rc = irsGetIrCameraHandle(&irhandle, CONTROLLER_PLAYER_1);
+		printf("irsGetIrCameraHandle() returned 0x%x\n", rc);
+	}
+
+	while(appletMainLoop())
+	{
+		//Scan all the inputs. This should be done once for each frame
+		hidScanInput();
+
+		if (hidKeysDown(CONTROLLER_P1_AUTO) & KEY_PLUS) break;
+
+		gfxFlushBuffers();
+		gfxSwapBuffers();
+		gfxWaitForVsync();
+	}
+
+	irsExit();
+
+	gfxExit();
+	return 0;
+}
+
