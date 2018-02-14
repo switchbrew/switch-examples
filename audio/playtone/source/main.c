9b0ce2f49899734c26590e2fd783165072ccb8dd
Mike H <mike.hexkyz@gmail.com>
2018-02-14 15:35:34 +0000

    Accommodate libnx changes

--- audio/playtone/source/main.c
+++ audio/playtone/source/main.c
@@ -19,30 +19,6 @@ void fill_audio_buffer(void* audio_buffer, size_t offset, size_t size, int frequ
     }
 }
 
-void audout_play(Handle *event, AudioOutBuffer *source_buffer, AudioOutBuffer *released_buffer)
-{
-    u64 time_now = svcGetSystemTick();
-    while ((svcGetSystemTick() - time_now) < 1250000)
-    {
-        s32 index;
-        Result do_wait = svcWaitSynchronization(&index, event, 1, 10000000);
-        
-        if (R_SUCCEEDED(do_wait))
-        {
-            svcResetSignal(*event);
-            
-            u32 released_count = 0;
-            Result do_release = audoutGetReleasedAudioOutBuffer(released_buffer, &released_count);
-            
-            while (R_SUCCEEDED(do_release) && (released_count > 0))
-            {
-                do_release = audoutGetReleasedAudioOutBuffer(released_buffer, &released_count);
-                audoutAppendAudioOutBuffer(source_buffer);
-            }
-        }
-    }
-}
-
 int main(int argc, char **argv)
 {
     Result rc = 0;
@@ -181,7 +157,7 @@ int main(int argc, char **argv)
         
         if (play_tone)
         {
-            audout_play(&event, &source_buffer, &released_buffer);
+            audoutPlayBuffer(&event, &source_buffer, &released_buffer, 1250000);
             play_tone = false;
         }
         
