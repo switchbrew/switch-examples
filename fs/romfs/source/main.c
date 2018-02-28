3693998cbc0c644e50918cec199997d6b474c69c
yellows8 <yellows8@users.noreply.github.com>
2018-02-28 11:57:24 -0500

    Added romfs example based on the 3ds-examples one, not yet ready for use.

---
+++ fs/romfs/source/main.c
@@ -0,0 +1,64 @@
+#include <string.h>
+#include <stdio.h>
+
+#include <switch.h>
+
+void printfile(const char* path)
+{
+	FILE* f = fopen(path, "r");
+	if (f)
+	{
+		char mystring[100];
+		while (fgets(mystring, sizeof(mystring), f))
+		{
+			int a = strlen(mystring);
+			if (mystring[a-1] == '\n')
+			{
+				mystring[a-1] = 0;
+				if (mystring[a-2] == '\r')
+					mystring[a-2] = 0;
+			}
+			puts(mystring);
+		}
+		printf(">>EOF<<\n");
+		fclose(f);
+	}
+}
+
+int main(int argc, char **argv)
+{
+	gfxInitDefault();
+	consoleInit(NULL);
+
+	Result rc = romfsInit();
+	if (rc)
+		printf("romfsInit: %08X\n", rc);
+	else
+	{
+		printf("romfs Init Successful!\n");
+		printfile("romfs:/folder/file.txt");
+		// Test reading a file with non-ASCII characters in the name
+		printfile("romfs:/フォルダ/ファイル.txt");
+	}
+
+	// Main loop
+	while(appletMainLoop())
+	{
+		//Scan all the inputs. This should be done once for each frame
+		hidScanInput();
+
+		//hidKeysDown returns information about which buttons have been just pressed (and they weren't in the previous frame)
+		u32 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+
+		if (kDown & KEY_PLUS) break; // break in order to return to hbmenu
+
+		gfxFlushBuffers();
+		gfxSwapBuffers();
+		gfxWaitForVsync();
+	}
+
+	romfsExit();
+	gfxExit();
+	return 0;
+}
+
