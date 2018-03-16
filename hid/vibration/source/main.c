226bae08e93532a5f26f45d8c76349c26ea09b1d
yellows8 <yellows8@users.noreply.github.com>
2018-03-15 22:26:50 -0400

    Use handheld-mode/player1-controller automatically in vibration example.

--- hid/vibration/source/main.c
+++ hid/vibration/source/main.c
@@ -4,12 +4,12 @@
 #include <switch.h>
 
 //Example for HID vibration/rumble.
-//TODO: Select CONTROLLER_HANDHELD/CONTROLLER_PLAYER_1 automatically instead of hard-coding to CONTROLLER_HANDHELD.
 
 int main(int argc, char **argv)
 {
-    u32 VibrationDeviceHandles[2];
+    u32 VibrationDeviceHandles[2][2];
     Result rc = 0, rc2 = 0;
+    u32 target_device=0;
 
     HidVibrationValue VibrationValue;
     HidVibrationValue VibrationValue_stop;
@@ -22,7 +22,10 @@ int main(int argc, char **argv)
 
     //Two VibrationDeviceHandles are returned: first one for left-joycon, second one for right-joycon.
     //Change the total_handles param to 1, and update the hidSendVibrationValues calls, if you only want 1 VibrationDeviceHandle.
-    rc = hidInitializeVibrationDevices(VibrationDeviceHandles, 2, CONTROLLER_HANDHELD, TYPE_HANDHELD | TYPE_JOYCON_PAIR);
+    rc = hidInitializeVibrationDevices(VibrationDeviceHandles[0], 2, CONTROLLER_HANDHELD, TYPE_HANDHELD | TYPE_JOYCON_PAIR);
+
+    //Setup VibrationDeviceHandles for CONTROLLER_PLAYER_1 too, since we want to support both CONTROLLER_HANDHELD and CONTROLLER_PLAYER_1.
+    if (R_SUCCEEDED(rc)) rc = hidInitializeVibrationDevices(VibrationDeviceHandles[1], 2, CONTROLLER_PLAYER_1, TYPE_HANDHELD | TYPE_JOYCON_PAIR);
     printf("hidInitializeVibrationDevices() returned: 0x%x\n", rc);
 
     if (R_SUCCEEDED(rc)) printf("Hold R to vibrate, and press A/B/X/Y while holding R to adjust values.\n");
@@ -52,6 +55,11 @@ int main(int argc, char **argv)
 
         if (kDown & KEY_PLUS) break; // break in order to return to hbmenu
 
+        //Select which devices to vibrate.
+        target_device = 0;
+        if (!hidGetHandheldMode())
+            target_device = 1;
+
         if (R_SUCCEEDED(rc) && (kHeld & KEY_R))
         {
             //Calling hidSendVibrationValue/hidSendVibrationValues is really only needed when sending new VibrationValue(s).
@@ -60,7 +68,7 @@ int main(int argc, char **argv)
             memcpy(&VibrationValues[0], &VibrationValue, sizeof(HidVibrationValue));
             memcpy(&VibrationValues[1], &VibrationValue, sizeof(HidVibrationValue));
 
-            rc2 = hidSendVibrationValues(VibrationDeviceHandles, VibrationValues, 2);
+            rc2 = hidSendVibrationValues(VibrationDeviceHandles[target_device], VibrationValues, 2);
             if (R_FAILED(rc2)) printf("hidSendVibrationValues() returned: 0x%x\n", rc2);
 
             if (kDown & KEY_A) VibrationValue.amp_low   += 0.1f;
@@ -68,13 +76,17 @@ int main(int argc, char **argv)
             if (kDown & KEY_X) VibrationValue.amp_high  += 0.1f;
             if (kDown & KEY_Y) VibrationValue.freq_high += 12.0f;
         }
-        else if(kUp & KEY_R)//Stop vibration.
+        else if(kUp & KEY_R)//Stop vibration for all devices.
         {
             memcpy(&VibrationValues[0], &VibrationValue_stop, sizeof(HidVibrationValue));
             memcpy(&VibrationValues[1], &VibrationValue_stop, sizeof(HidVibrationValue));
 
-            rc2 = hidSendVibrationValues(VibrationDeviceHandles, VibrationValues, 2);
+            rc2 = hidSendVibrationValues(VibrationDeviceHandles[target_device], VibrationValues, 2);
             if (R_FAILED(rc2)) printf("hidSendVibrationValues() for stop returned: 0x%x\n", rc2);
+
+            //Could also do this with 1 hidSendVibrationValues() call + a larger VibrationValues array.
+            rc2 = hidSendVibrationValues(VibrationDeviceHandles[1-target_device], VibrationValues, 2);
+            if (R_FAILED(rc2)) printf("hidSendVibrationValues() for stop other device returned: 0x%x\n", rc2);
         }
 
         gfxFlushBuffers();
