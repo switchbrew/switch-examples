888f86ab36a1f0f4dead0549037598f0befdbf39
yellows8 <yellows8@users.noreply.github.com>
2018-11-14 22:01:30 -0500

    Added exception-handler example.

---
+++ exception-handler/source/main.c
@@ -0,0 +1,78 @@
+// Include the most common headers from the C standard library
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Include the main libnx system header, for Switch development
+#include <switch.h>
+
+//This example shows how to use userland exception handling. See also libnx init.c and thread_context.h.
+
+alignas(16) u8 __nx_exception_stack[0x1000];
+u64 __nx_exception_stack_size = sizeof(__nx_exception_stack);
+
+void __libnx_exception_handler(ThreadExceptionDump *ctx)
+{
+    int i;
+    FILE *f = fopen("exception_dump", "w");
+    if(f==NULL)return;
+
+    fprintf(f, "error_desc: 0x%x\n", ctx->error_desc);//You can also parse this with ThreadExceptionDesc.
+    //This assumes AArch64, however you can also use threadExceptionIsAArch64().
+    for(i=0; i<29; i++)fprintf(f, "[X%d]: 0x%lx\n", i, ctx->cpu_gprs[i].x);
+    fprintf(f, "fp: 0x%lx\n", ctx->fp.x);
+    fprintf(f, "lr: 0x%lx\n", ctx->lr.x);
+    fprintf(f, "sp: 0x%lx\n", ctx->sp.x);
+    fprintf(f, "pc: 0x%lx\n", ctx->pc.x);
+
+    //You could print fpu_gprs if you want.
+
+    fprintf(f, "pstate: 0x%x\n", ctx->pstate);
+    fprintf(f, "afsr0: 0x%x\n", ctx->afsr0);
+    fprintf(f, "afsr1: 0x%x\n", ctx->afsr1);
+    fprintf(f, "esr: 0x%x\n", ctx->esr);
+
+    fprintf(f, "far: 0x%lx\n", ctx->far.x);
+
+    fclose(f);
+}
+
+// Main program entrypoint
+int main(int argc, char* argv[])
+{
+    // This example uses a text console, as a simple way to output text to the screen.
+    // If you want to write a software-rendered graphics application,
+    //   take a look at the graphics/simplegfx example, which uses the libnx gfx API instead.
+    // If on the other hand you want to write an OpenGL based application,
+    //   take a look at the graphics/opengl set of examples, which uses EGL instead.
+    consoleInit(NULL);
+
+    printf("Triggering crash...\n");
+    consoleUpdate(NULL);
+
+    //Trigger a crash.
+    *((u64*)8) = 16;
+
+    // Main loop
+    while (appletMainLoop())
+    {
+        // Scan all the inputs. This should be done once for each frame
+        hidScanInput();
+
+        // hidKeysDown returns information about which buttons have been
+        // just pressed in this frame compared to the previous one
+        u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+
+        if (kDown & KEY_PLUS)
+            break; // break in order to return to hbmenu
+
+        // Your code goes here
+
+        // Update the console, sending a new frame to the display
+        consoleUpdate(NULL);
+    }
+
+    // Deinitialize and clean up resources used by the console (important!)
+    consoleExit(NULL);
+    return 0;
+}
